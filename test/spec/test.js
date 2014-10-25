f2ca224406d95404eeedb4095f4529bc501de1df
m.sakamaki <m.sakamaki@gxp.co.jp>
2014-10-25 17:35:54 +0900

    add testcase

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,6 +1,7 @@
 /* global describe, it */
+import Dispacher from '../../app/scripts/dispacher/dispacher'
+import SingleList from '../../app/scripts/dispacher/singlelist'
 
-//import Dispacher from '../../app/scripts/dispacher'
 //var _disp = new Dispacher();
 
 
@@ -10,15 +11,31 @@ class TestCase {
   // Module別に分けてImportする単体テストのほうが、効率が良いもする。
   // とりあえずは、モジュール単位のテストしやすさを検証
   UnitGo() {
-    describe('Dispacher', () => {
-      it ('constractor', () => {
-        expect(1).to.equal(1);
+    describe('Dispache`s', () => {
+      describe('Dispacher', () => {
+        let dispacher = new Dispacher();
+        before(()=>{
+          dispacher.listeners = []; // そのまま使うとシングルトンが他のテストに影響与えるので置き換え
+          dispacher.isCall = false;
+          dispacher.addListener((flg)=> {
+            dispacher.isCall = true;
+          });
+        });
+        it ('call emit method', () => {
+          dispacher.emit('ghange!');
+          expect(dispacher.isCall).to.be.true;
+        });
+        it ('check listeners length after addListner method call', () => {
+          expect(dispacher.listeners.length).to.equal(1);
+        });
       });
-      it ('emit method', () => {
-        expect(1).to.equal(1);
-      });
-      it ('addListner method', () => {
-        expect(1).to.equal(1);
+      describe('singleList', () => {
+        it ('singlelist to be Array ', () => {
+          expect(SingleList.instance.listeners instanceof Array).to.be.true;
+        });
+        it ('throw error Cannot construct singleton', () => {
+          expect(1).to.equal(1);
+        });
       });
     });
     describe('Store', () => {
